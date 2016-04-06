a12a254bf0d4c5a4aca15e90e4a9ea2812ac9dd3
Ryan Harvey and Sam Durfey <sam@>
2016-04-06 13:57:30 -0700

    Added feedback to indicate current player's turn. fixed a bug involving passing turns on rolls of 1.

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 //Bidness
 var player1;
 var player2;
+var temporaryScore = 0;
+var yourTurn = "";
 
-function Player(turnYN, humanYN){
+function Player(name, turnYN, humanYN){
+	this["name"] = name;
 	this["permanentScore"] = 0;
-	this["temporaryScore"] = 0;
 	this["turn"] = turnYN;
 	this["human"] = humanYN;
 }
+Player.prototype.announceTurn = function(){
+	if(this.turn === true){
+		yourTurn = this.name;
+	}
+}
 
 Player.prototype.passTurn = function(){
-	this.temporaryScore = 0;
+	temporaryScore = 0;
 	if (this.turn === true) {
-		alert("Passing turn");
+		// this.announceTurn();
 		this.turn = false;
 	} else {
 		this.turn = true;
+		this.announceTurn();
 	}
 }
 
@@ -24,53 +32,16 @@ Player.prototype.dieRoll = function() {
 }
 
 Player.prototype.fullRoll = function() {
-	if(this.turn === true){
-		var roll = this.dieRoll();
-		alert(roll);
-		if(roll !== 1){
-			this.temporaryScore += roll;
-		} else if (roll === 1){
-			player1.passTurn();
-			player2.passTurn();
-		}
+	var roll = this.dieRoll();
+	alert(roll);
+	if(roll !== 1){
+		temporaryScore += roll;
+	} else if (roll === 1){
+		player1.passTurn();
+		player2.passTurn();
 	}
+	return roll;
 }
-
-// Player.prototype.computerTurn = function(){
-// 	while(player2.turn === true){
-// 		var rollComp = player2.dieRoll();
-// 		if(rollComp !== 1){
-// 			player2.temporaryScore += roll2;
-// 		} else if (rollComp === 1){
-// 			player1.passTurn();
-// 			player2.passTurn();
-// 		} else {
-// 		alert("ERRR BRUH");
-// 		}
-// 		var rollComp = player2.dieRoll();
-// 		if(rollComp !== 1){
-// 			player2.temporaryScore += roll2;
-// 		} else if (rollComp === 1){
-// 			player1.passTurn();
-// 			player2.passTurn();
-// 		} else {
-// 		alert("ERRR BRUH");
-// 		}
-// 		player1.permanentScore += player1.temporaryScore;
-// 		$("#player1Total").text(player1.permanentScore);
-// 		player2.permanentScore += player2.temporaryScore;
-// 		$("#player2Total").text(player2.permanentScore);
-// 		player1.passTurn();
-// 		player2.passTurn();
-// 		if (player1.permanentScore >= 100){
-// 			alert("Player 1 Wins");
-// 		}
-// 		if (player2.permanentScore >= 100){
-// 			alert("Player 2 Wins");
-// 		}
-// 	}
-// }
-
 
 //UI Logic
 
@@ -79,8 +50,8 @@ $(document).ready(function() {
 	$("#onePlayer").click(function(){
 		$(".gameType").hide();
 		$(".game").show();
-		player1 = new Player(true, true);
-		player2 = new Player(false, false);
+		player1 = new Player("Player 1", true, true);
+		player2 = new Player("Computer", false, false);
 	});
 
 	//if(player2.turn === true && player2.human === false){
@@ -91,50 +62,44 @@ $(document).ready(function() {
 	$("#twoPlayer").click(function(){
 		$(".gameType").hide();
 		$(".game").show();
-		player1 = new Player(true, true);
-		alert(player1.turn);
-		player2 = new Player(false, true);
+		player1 = new Player("Player 1", true, true);
+		player2 = new Player("Player 2", false, true);
+		player1.announceTurn();
+		$("#yourTurn").text(yourTurn);
 	});
-
 
 
 	$("#roll").click(function() {
-		player1.fullRoll();
-		player2.fullRoll();
-		// if(player1.turn === true){
-		// 	var roll = player1.dieRoll();
-		// 	alert(roll);
-		// 	if(roll !== 1){
-		// 		player1.temporaryScore += roll;
-		// 	} else if (roll === 1){
-		// 		player1.passTurn();
-		// 		player2.passTurn();
-		// 	}
-		// } else if (player2.turn === true){
-		// 	var roll2 = player2.dieRoll();
-		// 	alert(roll2);
-		// 	if(roll2 !== 1){
-		// 		player2.temporaryScore += roll2;
-		// 	} else if (roll2 === 1){
-		// 		player1.passTurn();
-		// 		player2.passTurn();
-		// 	}
-		// }
+		if (player1.turn === true) {
+			var lastRoll = player1.fullRoll();
+		} else {
+			var lastRoll = player2.fullRoll();
+		}
+		$("#tempScoreContainer").text(temporaryScore);
+		$("#lastRoll").text(lastRoll);
+		$("#yourTurn").text(yourTurn);
+
 	});
 
 	$("#pass").click(function() {
-		player1.permanentScore += player1.temporaryScore;
+		if (player1.turn === true) {
+			player1.permanentScore += temporaryScore;
+		} else if (player2.turn === true) {
+			player2.permanentScore += temporaryScore;
+		}
+
 		$("#player1Total").text(player1.permanentScore);
-		player2.permanentScore += player2.temporaryScore;
 		$("#player2Total").text(player2.permanentScore);
 		player1.passTurn();
 		player2.passTurn();
+		$("#yourTurn").text(yourTurn);
 		if (player1.permanentScore >= 100){
 			alert("Player 1 Wins");
 		}
 		if (player2.permanentScore >= 100){
 			alert("Player 2 Wins");
 		}
+		$("#tempScoreContainer").text(temporaryScore);
 	});
 
 	// if permanent score reaches 100, that player wins.
